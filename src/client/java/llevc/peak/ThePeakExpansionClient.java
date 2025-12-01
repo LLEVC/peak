cbbaa09286825a788faac1e7bb7984d43b6b3cc7
LLEVC <71854739+LLEVC@users.noreply.github.com>
2025-12-01 15:36:14 -0800

    the project file lol

---
+++ src/client/java/llevc/peak/ThePeakExpansionClient.java
@@ -0,0 +1,10 @@
+package llevc.peak;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThePeakExpansionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/llevc/peak/ThePeakExpansionDataGenerator.java
@@ -0,0 +1,11 @@
+package llevc.peak;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThePeakExpansionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
