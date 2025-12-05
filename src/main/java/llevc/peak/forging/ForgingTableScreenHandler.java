83f8781e9a3fd39247795a76139e463b7c09c67f
LLEVC <71854739+LLEVC@users.noreply.github.com>
2025-12-05 01:28:43 -0800

    updated forging
    -added custom crafting (although its hardcoded)
    -removed normal crafting

--- src/main/java/llevc/peak/forging/ForgingTableScreenHandler.java
+++ src/main/java/llevc/peak/forging/ForgingTableScreenHandler.java
@@ -1,16 +1,18 @@
 package llevc.peak.forging;
 
 import llevc.peak.ModBlocks;
+import llevc.peak.ModItems;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.CraftingResultInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.RecipeInputInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
 import net.minecraft.recipe.CraftingRecipe;
 import net.minecraft.recipe.RecipeEntry;
-import net.minecraft.recipe.RecipeType;
 import net.minecraft.recipe.input.CraftingRecipeInput;
 import net.minecraft.screen.CraftingScreenHandler;
 import net.minecraft.screen.ScreenHandler;
@@ -19,7 +21,9 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class ForgingTableScreenHandler extends CraftingScreenHandler {
     private final ScreenHandlerContext context;
@@ -42,20 +46,119 @@ public class ForgingTableScreenHandler extends CraftingScreenHandler {
         return ScreenHandler.canUse(context, player, ModBlocks.ForgingTable);
     }
 
+    public boolean slotIsOf(ScreenHandler handler, int index,Item item) {
+        return handler.getSlot(index).getStack().isOf(item);
+    }
+
     protected static void updateResult(ScreenHandler handler, ServerWorld world, PlayerEntity player, RecipeInputInventory craftingInventory, CraftingResultInventory resultInventory, @Nullable RecipeEntry<CraftingRecipe> recipe) {
         CraftingRecipeInput craftingRecipeInput = craftingInventory.createRecipeInput();
         ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)player;
         ItemStack itemStack = ItemStack.EMPTY;
-        Optional<RecipeEntry<CraftingRecipe>> optional = world.getServer().getRecipeManager().getFirstMatch(RecipeType.CRAFTING, craftingRecipeInput, world, recipe);
-        if (optional.isPresent()) {
-            RecipeEntry<CraftingRecipe> recipeEntry = (RecipeEntry<CraftingRecipe>)optional.get();
-            CraftingRecipe craftingRecipe = recipeEntry.value();
-            if (resultInventory.shouldCraftRecipe(serverPlayerEntity, recipeEntry)) {
-                ItemStack itemStack2 = craftingRecipe.craft(craftingRecipeInput, world.getRegistryManager());
-                if (itemStack2.isItemEnabled(world.getEnabledFeatures())) {
-                    itemStack = itemStack2;
+
+        Item material = handler.getSlot(2).getStack().getItem();
+        Item rod = handler.getSlot(8).getStack().getItem();
+        Item air = Items.AIR;
+
+        List<Item> swordPattern = List.of(
+                air, material, air,
+                air, material, air,
+                air, rod, air
+        );
+        List<Item> pickaxePattern = List.of(
+                material, material, material,
+                air, rod, air,
+                air, rod, air
+        );
+        List<Item> axePattern = List.of(
+                material, material, air,
+                material, rod, air,
+                air, rod, air
+        );
+        List<Item> shovelPattern = List.of(
+                air, material, air,
+                air, rod, air,
+                air, rod, air
+        );
+        List<Item> hoePattern = List.of(
+                material, material, air,
+                air, rod, air,
+                air, rod, air
+        );
+        Iterator<List<Item>> yes = List.of(
+                swordPattern, // 1 Sword
+                pickaxePattern, // 2 Pick
+                axePattern, // 3 Axe
+                shovelPattern, // 4 Shovel
+                hoePattern // 5 Hoe
+        ).iterator();
+
+        boolean sword = false;
+        boolean pickaxe = false;
+        boolean axe = false;
+        boolean shovel = false;
+        boolean hoe = false;
+        boolean valid = false;
+
+        List<Item> materials = List.of(Items.IRON_INGOT,Items.GOLD_INGOT,Items.DIAMOND,Items.EMERALD);
+        List<Item> rods = List.of(Items.STICK);
+
+        if (materials.contains(material) && rods.contains(rod)) {
+            int j = 1;
+            while (yes.hasNext()) {
+                List<Item> pattern = yes.next();
+                Iterator<Item> heyNow = pattern.iterator();
+                int i = 0;
+                final boolean[] heyo = {
+                        false, false, false,
+                        false, false, false,
+                        false, false, false
+                };
+                while (heyNow.hasNext()) {
+                    Item wait1Sec = heyNow.next();
+                    Item wait2Sec = handler.getSlot(i + 1).getStack().getItem();
+                    if (wait1Sec.equals(wait2Sec) || (wait1Sec.equals(Items.AIR) && handler.getSlot(i + 1).getStack().isEmpty())) {
+                        heyo[i] = true;
+                    }
+                    i = i + 1;
                 }
+                if (Arrays.equals(heyo, new boolean[]{
+                        true, true, true,
+                        true, true, true,
+                        true, true, true
+                })) {
+                    switch (j) {
+                        case 1:
+                            sword = true;
+                        case 2:
+                            pickaxe = true;
+                        case 3:
+                            axe = true;
+                        case 4:
+                            shovel = true;
+                        case 5:
+                            hoe = true;
+                    }
+                    valid = true;
+                    break;
+                }
+                j = j + 1;
             }
+        }
+
+        if (valid) {
+            ItemStack itemStack2 = null;
+            if (sword) {
+                itemStack2 = ModItems.ModularSwordItem.getDefaultStack();
+            } else if (pickaxe) {
+                itemStack2 = ModItems.ModularSwordItem.getDefaultStack();
+            }  else if (axe) {
+                itemStack2 = ModItems.ModularSwordItem.getDefaultStack();
+            } else if (shovel) {
+                itemStack2 = ModItems.ModularSwordItem.getDefaultStack();
+            } else if (hoe) {
+                itemStack2 = ModItems.ModularSwordItem.getDefaultStack();
+            }
+            itemStack = itemStack2;
         }
 
         resultInventory.setStack(0, itemStack);
